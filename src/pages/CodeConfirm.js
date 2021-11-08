b30c42bf2d83deb8b46c087f64dd013714ef03d2
siddhant gore <siddhantgore111@gmail.com>
2021-11-08 18:56:33 +0530

    CodeConfirm page added

---
+++ src/pages/CodeConfirm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from "../style/phoneConfirmation.module.css";
+import {Link} from "react-router-dom";
+
+function CodeConfirm() {
+    return (
+        <div className={style.phoneConfirmationContainer}>
+            <Link exact to="/invite" className={style.backBtn}>
+                <img src="/images/back_arrow.svg" alt=""/>
+            </Link>
+            <div className="text-center">
+               <h2>Enter Confirmation Code</h2> 
+               <input type="text"
+               style={{
+                   textAlign:"center",
+                   border:"none",
+                   outline:"none",
+               }}
+               />
+               <p className="mt-2">Didn't Recieve Code? <span>Resend</span></p>
+            </div>
+            <Link exact to="code_confirm" className="primaryBtn">
+                Next <img src="" className="ml-1"/>
+            </Link>
+        </div>
+    )
+}
+
+export default CodeConfirm
