52d9891da3f8d5d351b5c5cc1f4e8c0c4a64ff4a
siddhant gore <siddhantgore111@gmail.com>
2021-11-08 18:57:29 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import Welcome from './pages/Welcome';
+import PlanLayout from './pages/Layouts/PlanLayout';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import PhoneConfirmation from './pages/PhoneConfirmation';
+import CodeConfirm from './pages/CodeConfirm';
 
 function App() {
   return (
-   <Welcome/>
+    <BrowserRouter>
+      <PlanLayout>
+      <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/invite" element={<PhoneConfirmation />} />
+      <Route path="/invite/code_confirm" element={<CodeConfirm />} />
+    </Routes>
+      </PlanLayout>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from "../style/welcome.module.css"
+import {Link} from "react-router-dom"
 
 function Welcome() {
     return (
@@ -14,6 +15,13 @@ function Welcome() {
                     Since launching in 2020 we’ve gone from Seed to Series A, B and C — backed by an amazing and diverse group, including a16z and many of the top angel investors and creators in the world.
                 </p>
                 <p>-The Clubhouse Team</p>
+            </div>
+            <div className={style.actionBtn}>
+                <Link exact to="/invite" className="primaryBtn d-flex align-items-center mb-3">
+                    Get Username{""}
+                    {/* <img src="" alt=""/> */}
+                </Link>
+                <Link exact to="/sign_in">Haven't an invite text? Sign in</Link>
             </div>
         </div>
     )
