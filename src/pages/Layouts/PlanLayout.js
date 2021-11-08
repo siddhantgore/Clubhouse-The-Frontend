ef220683d736f3bc31532c6282f19576401d1390
siddhant gore <siddhantgore111@gmail.com>
2021-11-08 18:53:24 +0530

    Layout added

---
+++ src/pages/Layouts/PlanLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlanLayout({children}) {
+    return (
+        <div style={{width:"100%",margin:"0"}}>
+            {children}
+        </div>
+    )
+}
+
+export default PlanLayout
