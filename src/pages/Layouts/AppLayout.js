864007964fb78de7767a4534c25a3dcaa54c8ee8
siddhant gore <siddhantgore111@gmail.com>
2021-11-12 12:04:43 +0530

    AppLayout added

---
+++ src/pages/Layouts/AppLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppLayout({children}) {
+    return (
+        <div className="appLayout">
+            {children}
+        </div>
+    )
+}
+
+export default AppLayout
