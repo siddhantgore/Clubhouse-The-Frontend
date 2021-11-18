6effb3bd62c179054d33472c1e190423c8b5df3a
siddhant gore <siddhantgore111@gmail.com>
2021-11-18 15:52:54 +0530

    RoominfoCard component added

---
+++ src/components/RoominfoCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "../style/roomCard.module.css";
+import data from "../data/roomCard.json";
+
+
+function roominfoCard() {
+    return (
+        <div>
+            This is room info card
+        </div>
+    )
+}
+
+export default roominfoCard
