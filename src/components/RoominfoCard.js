830372d4cac04a5b3299aa0e2a0ed418378b2a6a
siddhant gore <siddhantgore111@gmail.com>
2021-11-21 22:49:38 +0530

    updated

--- src/components/RoominfoCard.js
+++ src/components/RoominfoCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from "../style/roomCard.module.css";
 import data from "../data/roomCard.json";
-
+import {BsChatDots,BsChatDotsFill,BsFillPersonFill} from "react-icons/bs";
 
 function RoominfoCard() {
     return (
@@ -15,9 +15,18 @@ function RoominfoCard() {
                            <div>
                                {items.members.map((members)=>(
                                    <p>
-                                       {members.fname} {members.lname}
+                                       {members.fname} {members.lname} <BsChatDots/>
                                    </p>
                                ))}
+                               <p className="icon">
+                                   <span className="mr-2">1.8</span>
+                                   <span className="mx-2"></span>{" "}
+                                   <BsChatDotsFill/>
+                                   <span className="mx-2"></span>{"  "}
+                                   <span className="mr-2">5</span>
+                                   <span className="mx-2"></span>{" "}
+                                   <BsFillPersonFill/>
+                               </p>
                            </div>
                        </div>
                    </div>
