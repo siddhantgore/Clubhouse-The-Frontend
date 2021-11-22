f1d983d5f7a8d0d1ad9aaae3a484ffe3006f749e
siddhant gore <siddhantgore111@gmail.com>
2021-11-22 22:24:21 +0530

    BottomSheet component added

---
+++ src/components/BottomSheet.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
+import style from "../style/bottomSheet.module.css";
+import data from "../data/roomCard.json";
+
+function BottomSheet() {
+    return (
+        <SwipeableBottomSheet
+        open={props.sheetVisible}
+        onChange={()=>{
+            props.setSheetVisible(!props.sheetVisible)
+            props.setItemsVisible(true)
+        }
+        }
+        fullScreen={props.sheetTitle=='room details' ? true: false}
+        >
+        <div className={style.BottomSheetContainer}
+        style={{
+            backgroundColor:props.sheetTitle=='profile' ? 'transparent': ''
+        }}
+        >
+        </div>
+        </SwipeableBottomSheet>
+    )
+}
+
+export default BottomSheet
